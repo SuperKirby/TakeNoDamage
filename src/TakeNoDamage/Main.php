24a553961ba86447ff1925ea8329db7139f169ad
Syntaxious <SynPlaysMC@gmail.com>
2016-04-09 21:22:13 +0100

    Update Main.php

--- src/TakeNoDamage/Main.php
+++ src/TakeNoDamage/Main.php
@@ -10,9 +10,11 @@ class Main extends PluginBase implements Listener {
 public function onEnable() {
     $this->getLogger()->info("TakeNoDamage enabled!");
     }
+    
 public function onDisable() {
     $this->getlogger()->info("Take no damage enabled!");       
     }
+    
 public function onDamage(EntityDamageEvent $event) {
   if($event->getCause() === EntityDamageEvent::CAUSE_FALL){
     $event->setCancelled();
